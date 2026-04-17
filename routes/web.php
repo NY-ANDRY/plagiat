f6ea58a0e6ff47efe94cb3ab475b0e6031f4fef4
NY-ANDRY <nyandrypaulferdinah@gmail.com>
2026-04-17 14:13:44 +0300

    put middleware example

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,10 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('/admin', fn () => 'Bienvenue sur l’espace admin')
+        ->middleware('role:admin')
+        ->name('admin.dashboard');
 });
 
 require __DIR__.'/auth.php';
